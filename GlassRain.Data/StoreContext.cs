2c653276c2ee8b0d6e917986ccd53371d1f37f8a
Isaac Tan <27454it@gmail.com>
2025-11-09 01:29:48 -0500

    Adding other changes from section 1-3

---
+++ GlassRain.Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using GlassRain.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlassRain.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        { }
+        
+        public DbSet<Item> Items { get; set; }
+    }
+
+}
