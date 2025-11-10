4c67a1218f2239d928ba08c83df63b625b64a06e
Vivian Wang <wang.15693@osu.edu>
2025-11-09 19:12:44 -0500

    Adding Get By Id

--- GlassRain.Domain/Catalog/Rating.cs
+++ GlassRain.Domain/Catalog/Rating.cs
@@ -4,6 +4,7 @@ namespace GlassRain.Domain.Catalog
 {
     public class Rating
     {
+        public int Id { get; set; }
         public int Stars { get; set; }
         public string UserName { get; set; }
         public string Review { get; set; }
